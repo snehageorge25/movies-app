76b58f711a33eba0a278d5d3a5f153f1d9e8426c
snehageorge25 <esssneha@gmail.com>
2023-05-30 18:08:32 +0530

    Adding files

---
+++ src/components/MoviePage/MoviePage.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getMovieDetails, getSimilarMovies } from "../../api/movies";
+
+import MovieCard from "../MovieCard/MovieCard";
+
+import styles from "./MoviePage.module.css";
+import Paginate from "../Paginate/Paginate";
+
+const imagePrefixUrl = "https://image.tmdb.org/t/p/w500";
+
+function MoviePage() {
+  const params = useParams();
+  const movieId = params.movieId;
+  const [movie, setMovie] = useState({});
+  const [similarMovies, setSimilarMovies] = useState([]);
+
+  const fetchMovieDetails = () => {
+    getMovieDetails(movieId).then((res) => {
+      if (!res) return;
+      setMovie(res);
+    });
+  };
+
+  const fetchSimilarMovies = (page) => {
+    getSimilarMovies(movieId).then((res) => {
+      if (!res) return;
+      setSimilarMovies(res.results);
+    });
+  };
+
+  useEffect(() => {
+    fetchMovieDetails();
+    fetchSimilarMovies(1);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <img src={`${imagePrefixUrl}${movie?.backdrop_path}`} alt="" />
+        <div className={styles.details}>
+          <label>Title</label>
+          <div className={styles.title}>{movie?.title}</div>
+          <div className={styles.sub}>{movie?.tagline}</div>
+          <label>Story</label>
+          <div className={styles.desc}>{movie?.overview}</div>
+          <label>Runtime</label>
+          <div className={styles.desc}>{movie?.runtime} mins</div>
+        </div>
+      </div>
+      <div className={styles.similar}>
+        <p className={styles.title}>Similar Movies</p>
+        <div className={styles.movies}>
+          {similarMovies.map((item) => (
+            <MovieCard movie={item} key={item.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MoviePage;
